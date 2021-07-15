513f4f018c07a3d897c01037e2a03fda4a7a173f
Suvrata Kumar Hada <suvrat777@gmail.com>
2021-07-15 14:23:06 +0530

    Update CorsConfig.java

--- reactivePrograming/src/main/java/com/rt/boot/config/CorsConfig.java
+++ reactivePrograming/src/main/java/com/rt/boot/config/CorsConfig.java
@@ -8,11 +8,10 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
 @Configuration
 @EnableWebFlux
 public class CorsConfig implements WebFluxConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry corsRegistry) {
-        corsRegistry.addMapping("/**")
+	@Override
+	public void addCorsMappings(CorsRegistry corsRegistry) {
+		corsRegistry.addMapping("/**")
 //                .allowedOrigins("http://127.0.0.1:5500", "http://127.0.0.1:5500")
-                .allowedMethods("GET")
-                .maxAge(3600);
-    }
+				.allowedMethods("GET").maxAge(3600);
+	}
 }
