d613bf31cb936523566e1e77a4bd57ddc6397206
Suvrata Kumar Hada <suvrat777@gmail.com>
2021-07-23 23:09:56 +0530

    Update ProductHandler.java

--- reactivePrograming/src/main/java/com/rt/boot/handdler/ProductHandler.java
+++ reactivePrograming/src/main/java/com/rt/boot/handdler/ProductHandler.java
@@ -15,7 +15,6 @@ import com.rt.boot.dto.ProdDto;
 import com.rt.boot.repo.ProductRepository;
 import com.rt.boot.utils.Response;
 
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -25,22 +24,22 @@ public class ProductHandler {
 	private ProductRepository productRepository;
 	
 	public Mono<ServerResponse> getAllProd(ServerRequest request){
-		Flux<Product> prods = productRepository.findAll().log().delayElements(Duration.ofSeconds(1));
-		Flux<ProdDto> prod = prods.map(this::productDtoMappig);
+		var prods = productRepository.findAll().log().delayElements(Duration.ofSeconds(1));
+		var prod = prods.map(this::productDtoMappig);
 		System.out.println(prod);
 		return ServerResponse.ok().contentType(MediaType.TEXT_EVENT_STREAM).body(prod, ProdDto.class);
 	}
 	
 	public Mono<ServerResponse> getById(ServerRequest request){
-		String id = request.pathVariable("id");
-		Mono<Product> pr = productRepository.findById(new ObjectId(id)).log();
-		Mono<ProdDto> mono = pr.map(this::productDtoMappig);
+		var id = request.pathVariable("id");
+		var pr = productRepository.findById(new ObjectId(id)).log();
+		var mono = pr.map(this::productDtoMappig);
 		return ServerResponse.ok().body(mono, ProdDto.class);
 	}
 	
 	public Mono<ServerResponse> save(ServerRequest request){
-		Mono<Product> prod = request.bodyToMono(ProdDto.class).flatMap(p -> {
-			Product product = new Product();
+		var prod = request.bodyToMono(ProdDto.class).flatMap(p -> {
+			var product = new Product();
 			BeanUtils.copyProperties(p, product);
 			return productRepository.save(product).log();
 		});
@@ -48,8 +47,8 @@ public class ProductHandler {
 	}
 	
 	public Mono<ServerResponse> update(ServerRequest request){
-		Mono<Product> prod = request.bodyToMono(ProdDto.class).flatMap(p -> {
-			Product product = new Product();
+		var prod = request.bodyToMono(ProdDto.class).flatMap(p -> {
+			var product = new Product();
 			BeanUtils.copyProperties(p, product);
 			product.setId(new ObjectId(p.getId()));
 			return productRepository.save(product).log();
@@ -58,7 +57,7 @@ public class ProductHandler {
 	}
 	
 	public Mono<ServerResponse> delete(ServerRequest request){
-		String id = request.pathVariable("id");
+		var id = request.pathVariable("id");
 		return ServerResponse.ok().body(productRepository.deleteById(new ObjectId(id)).log(), Response.class);
 	}
 	
